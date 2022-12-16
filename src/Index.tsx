9a6563cb957913fa277b5bf42381f918fb3b715a
Nick <nicolas@MacBook-Pro.local>
2022-12-16 14:18:56 +0200

    first commit

---
+++ src/Index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+
+const root = document.getElementById('root');
+
+if (root) {
+	createRoot(root).render(<App />);
+}
